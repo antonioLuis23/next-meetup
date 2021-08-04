48e13100cb3848e2b9e9ca8265681c18ae9fd59d
antonioLuis23 <antonio_luis@outlook.com>
2021-08-04 20:37:49 -0300

    Adicionando barra de navegação.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "Primeiro encontro",
+    image:
+      "https://www.tratabrasil.org.br/blog/wp-content/uploads/2020/08/teresina.jpg",
+    address: "Endereço 5, 14545 Teresina",
+    description: "O nosso primeiro encontro",
+  },
+  {
+    id: "m2",
+    title: "Segundo encontro",
+    image:
+      "https://www.tratabrasil.org.br/blog/wp-content/uploads/2020/12/belo-horizonte-lideranca-climatica-1103.jpg",
+    address: "Endereço 10, 14545 Belo Horizonte",
+    description: "O nosso segundo encontro",
+  },
+];
 const HomePage = () => {
-  return <MeetupList />;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
